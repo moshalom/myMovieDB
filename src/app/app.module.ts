4d95fe06ece8a331380388a30b6fc4b1e05ab8c0
shalom.moshe1@gmail.com <shalom.moshe1@gmail.com>
2018-12-03 12:31:20 +0200

    v.22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieComponent } from './movie/movie.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MovieOverviewComponent } from './movie-overview/movie-overview.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    MovieComponent,
+    MovieOverviewComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
